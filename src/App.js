1ec153f8e3984da2e11757f587c8ef067d3deb4c
harshendra kumar upadhyay <hu1201@exo.att.com>
2023-01-04 18:03:44 +0530

    making cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 import Header from './components/Header/index';
 import HeaderSlider from './components/Slider';
+import Playingcard from './components/Playingcard';
+import Promotioncards from './components/Promotioncards';
 
 function App() {
   return (
     // <div>hello</div>
   <><Header/>
    <HeaderSlider/>
+   <Playingcard/>
+   <Promotioncards/>
   </>
   );
 }
